fd385f1635fb8117c153059fae1731489c47a682
SMcGoey <SpencerCodeAcc@gmail.com>
2024-07-20 23:10:25 -0400

    set up starter code and installed all dependencies for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Home from "./components/Home.jsx"
+
+function App() {
+
+
+  return (
+    <div>
+
+      <Home />
+
+    </div>
+  )
+}
+
+export default App
