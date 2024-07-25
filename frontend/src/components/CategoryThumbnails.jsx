7539f541050f06b957e9b7844884bdee4b52acb5
SMcGoey <SpencerCodeAcc@gmail.com>
2024-07-24 23:03:24 -0400

    front end component strategize

---
+++ frontend/src/components/CategoryThumbnails.jsx
@@ -0,0 +1,7 @@
+
+const CategoryThumbnails = () => {
+    <div>
+        <p>either an img or a colour as the background</p>
+        <h2>Name of genre</h2>
+    </div>
+}
---
+++ frontend/src/components/PlaylistLibraryItem.jsx
@@ -0,0 +1,9 @@
+const PlayListLibraryItem =() =>{
+    return(
+        <div>
+            <img src="" alt="" /> this will be the image of the playlist that they make \\ set a default background colour for when they first make it and they haven't set an image
+            <p>This will be fetched playlist name</p>
+            <p>This will be the fetched playlist song total or have a hard coded paragraph that says PLAYLIST</p>
+        </div>
+    )
+}
