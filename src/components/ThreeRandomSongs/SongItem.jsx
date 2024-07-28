e329ff5f8b6370bd64234e7a8a5d4befd13fb9b3
SMcGoey <SpencerCodeAcc@gmail.com>
2024-07-28 14:36:58 -0400

    three random songs working

---
+++ src/components/ThreeRandomSongs/SongItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongItem = ({ songdata }) => {
+  return (
+    <div className='song-item-card'>
+      <img src={songdata.artworkUrl100} alt={songdata.trackName} />
+      <h2>{songdata.trackName}</h2>
+      <h4>{songdata.primaryGenreName}</h4>
+    </div>
+  );
+};
+
+export default SongItem;
+
