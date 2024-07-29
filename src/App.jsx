fb8ed7e65f5aaaef8b08c4573f97668bfecd6e5e
SMcGoey <SpencerCodeAcc@gmail.com>
2024-07-29 19:31:48 -0400

    commit 2 of hte day

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar'
 import Home from './pages/Home';
 import PlaylistShowPage from './components/Playlist/PlaylistShowPage';
 import CategoryShowPage from './components/Categories/Category/CategoryShowPage';
@@ -9,11 +10,16 @@ import { SongPlayerProvider } from './components/SongPlayer/SongPlayerContext';
 const App = () => {
   return (
     <SongPlayerProvider> 
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ marginLeft: '250px', flexGrow: 1, padding: '20px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/category/:genre" element={<CategoryShowPage />} />
           <Route path="/playlist/:id" element={<PlaylistShowPage />} />
         </Routes>
+        </div>
+        </div>
     </SongPlayerProvider>
   );
 };
