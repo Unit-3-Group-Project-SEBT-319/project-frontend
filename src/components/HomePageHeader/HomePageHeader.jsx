e7f07e4f99edfc5e34b5c842fd6a59c1fff6f2a1
SMcGoey <SpencerCodeAcc@gmail.com>
2024-08-18 02:56:50 -0400

    added stuff

--- src/components/HomePageHeader/HomePageHeader.jsx
+++ src/components/HomePageHeader/HomePageHeader.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import SearchBarContainer from "../SearchBar/SearchBarContainer";
 import './homepageheader.css';
 
@@ -10,10 +11,14 @@ const HomePageHeader = () => {
                 </div>
             </div>
             <div className="row justify-content-center align-items-center pt-5 header-logo">
-                <div className="col-auto text-center">
+                <div className="col-lg-6 col-md-7 col-sm-12 text-center">
                     <div className="logo">
-                        <h1 className="simplymusic">Simply</h1><span className="music">Music</span>
+                        <h1 className="simply">Simply</h1>
+                        <h1 className="music">Music.</h1>
                     </div>
+                </div>
+                <div className="col-lg-6 col-md-5 col-sm-12 text-center pic-div">
+                    <img src="/pictures/audiy-header-logo.png" className="header-image" alt="Logo" />
                 </div>
             </div>
         </div>
